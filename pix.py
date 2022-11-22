1a5da29687383c3d5cc4e512ebe314c890bdfd7c
BAN03 <angeldejesusatanasiodiaz@gmail.com>
2022-11-22 12:14:27 -0600

    test write pixels & np

--- pix.py
+++ pix.py
@@ -1,7 +1,12 @@
 import cv2
 from PIL import Image
+import random
+import numpy as np
 
 img = cv2.imread("win.png",3)
+
+def ran():
+    return random.randint(0,255)
 
 def showImage():
     pixel = Image.open("pxwin.png")
@@ -9,7 +14,7 @@ def showImage():
 
 def writePixel(height:int, width:int=None):
     if num2:
-        cv2.imwrite("pxwin.png",img[height][width])
+        cv2.imwrite("pxwin.png",img[height, width])
         showImage()
         return height, width
     else:
@@ -17,9 +22,18 @@ def writePixel(height:int, width:int=None):
         showImage()
         return height
 
+img_to_leds = np.ndarray((16, 16, 3), dtype = int)
+print(img_to_leds)
 
-
+"""
+img_to_leds = [[[0,0,0] for i in range(16)] for i in range(16)]
+print(type(img), type(img_to_leds))
+img_to_leds[5,5] = img[5,5]
 #print(img[writePixel(1,2)])
-print(img[5][5])
-cv2.imwrite("pxwin.png",img)
+print(img[6:8, 3])
+img[5, 5] = [14, 201, 25]
+#img[6:8] = [[ran(),ran(),ran()] for i in range(16)]
+img[6:8, 3] = [[25, 67, 240],[83, 94, 0]]
+cv2.imwrite("pxwin.png",img_to_leds)
 showImage()
+"""
