2c7eb1d162f995338d16d544901a89655c189560
BAN03 <angeldejesusatanasiodiaz@gmail.com>
2022-11-24 14:56:02 -0600

    img array to json file

--- pix.py
+++ pix.py
@@ -1,44 +1,22 @@
-import random
+import json
+from json import JSONEncoder
 import cv2
 from PIL import Image
 import numpy as np
 
 img = cv2.imread("win.png",3)
 
-def ran():
-    return random.randint(0,255)
-
 def showImage():
     pixel = Image.open("pxwin.png")
     pixel.show()
 
-def writePixel(height:int, width:int=None):
-    if width:
-        cv2.imwrite("pxwin.png",img[height, width])
-        showImage()
-        return height, width
-    else:
-        cv2.imwrite("pxwin.png",img[height])
-        showImage()
-        return height
+npjson = {"nparray":img.tolist()}
 
-img_to_leds = np.zeros((16, 16, 3), dtype = int)
-print(img_to_leds)
-x,y,z = img[5,5]
-for i in range(16):
-    for j in range(16):
-        img_to_leds[i,j] = img[i,j]
-"""
-img_to_leds = [[[0,0,0] for i in range(16)] for i in range(16)]
-print(type(img), type(img_to_leds))
-img_to_leds[5,5] = img[5,5]
-#print(img[writePixel(1,2)])
-print(img[6:8, 3])
-img[5, 5] = [14, 201, 25]
-#img[6:8] = [[ran(),ran(),ran()] for i in range(16)]
-img[6:8, 3] = [[25, 67, 240],[83, 94, 0]]
-cv2.imwrite("pxwin.png",img_to_leds)
-showImage()
-"""
+with open("img.json", "w") as file:
+    json.dump(npjson, file)
 
-print(x,y,z)
+with open("img.json", "r") as target:
+    img_to_leds = np.asarray(json.load(target)["nparray"])
+
+
+print(img_to_leds[5,5])
