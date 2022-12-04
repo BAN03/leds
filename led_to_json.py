3dd1698a1cee7b23500a4fd311e1d4f84acd6243
BAN03 <angeldejesusatanasiodiaz@gmail.com>
2022-12-04 14:59:58 -0600

    final product

--- led_to_json.py
+++ led_to_json.py
@@ -1,22 +1,20 @@
 import json
-from json import JSONEncoder
 import cv2
-from PIL import Image
-import numpy as np
 
-img = cv2.imread("win.png",3)
+img = cv2.imread(f"{input('Introduce el nombre del archivo: ')}.png",3)
+def reorderRGB(img_array):
+    lista = []
+    for i in range(16):
+        for j in range(16):
+            img_array[i][j][0], img_array[i][j][2] = img_array[i][j][2], img_array[i][j][0]
+            lista.append(img_array[i][j])
 
-def showImage():
-    pixel = Image.open("pxwin.png")
-    pixel.show()
+    njson = {
+        "array":lista
+    }
+    with open("img.json","w") as file:
+        json.dump(njson, file)
 
-npjson = {"nparray":img.tolist()}
-
-with open("img.json", "w") as file:
-    json.dump(npjson, file)
-
-with open("img.json", "r") as target:
-    img_to_leds = np.asarray(json.load(target)["nparray"])
-
-
-print(img_to_leds[5,5])
+njson = {"array":img.tolist()}
+reorderRGB(njson["array"])
+print("\narchivo img.json creado")
